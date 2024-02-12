c1e528e7d9e5376ef7190b9b7ea298855d26f55d
Faruq Bakare <bakarefaruq.a@gmail.com>
2024-02-12 19:14:32 +0100

    another banger

---
+++ app/roots/open-methods/systems-of-nonlinear-equations/newton-raphson/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      This one involves crammer's algorithm so I'm still trying to figure it out
+    </div>
+  );
+}
